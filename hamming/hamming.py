fd8ca62573224857c0305749c3c4887d685e59f6
Masato Hatayama <masato.hatayama@gmail.com>
2019-11-09 20:24:58 +0900

    applies mentor's advice

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,5 +1,17 @@
 def distance(strand_a, strand_b):
+    """Gets the differences between two strands of DNA
+
+    Args:
+        strand_a (str): The string of the first strand
+        strand_b (str): The string of the second strand
+
+    Returns:
+        int: A number of the differences between two strands
+
+    Raises:
+        ValueError: An error occurred two strands have different lengths.
+    """
     if len(strand_a) != len(strand_b):
-        raise ValueError("invalid number")
-    return len([x for x in range(0, len(strand_a))
-                if strand_a[x] != strand_b[x]])
+        raise ValueError("Two strands have different lengths.")
+    diffs = [x for x in range(0, len(strand_a)) if strand_a[x] != strand_b[x]]
+    return len(diffs)
