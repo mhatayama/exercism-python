e70da88b9a39242ccda4442d59cfc6ccbb7a45af
Masato Hatayama <masato.hatayama@gmail.com>
2019-11-09 20:25:29 +0900

    adds comment

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,4 +1,14 @@
 def steps(number, cnt=0):
+    """Gets and prints the spreadsheet's header columns
+
+    Args:
+        file_loc (str): The file location of the spreadsheet
+        print_cols (bool): A flag used to print the columns to the console
+            (default is False)
+
+    Returns:
+        list: a list of strings representing the header columns
+    """
     if number < 1:
         raise ValueError("invalid")
     elif number == 1:
