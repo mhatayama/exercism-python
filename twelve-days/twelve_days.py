45f241cb82946ebe2f6ecd4a524d724b96c7f12c
Masato Hayayama <masato.hatayama@gmail.com>
2020-05-23 11:58:43 +0900

    submit

---
+++ twelve-days/twelve_days.py
@@ -0,0 +1,30 @@
+ORDERS = ["first", "second", "third", "fourth",
+          "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+GIFTS = ["a Partridge in a Pear Tree.",
+         "two Turtle Doves",
+         "three French Hens",
+         "four Calling Birds",
+         "five Gold Rings",
+         "six Geese-a-Laying",
+         "seven Swans-a-Swimming",
+         "eight Maids-a-Milking",
+         "nine Ladies Dancing",
+         "ten Lords-a-Leaping",
+         "eleven Pipers Piping",
+         "twelve Drummers Drumming"]
+
+
+def recite(start_verse, end_verse):
+    return [create_song(num) for num in range(start_verse, end_verse + 1)]
+
+
+def create_song(num):
+    return "On the {} day of Christmas my true love gave to me: {}".format(
+        ORDERS[num - 1], create_gifts(num))
+
+
+def create_gifts(num):
+    gifts = [GIFTS[i] for i in range(num - 1, -1, -1)]
+    if num > 1:
+        gifts[num - 1] = "and " + gifts[num - 1]
+    return ", ".join(gifts)
