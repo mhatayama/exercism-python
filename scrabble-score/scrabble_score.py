1615941784b3477644421c8a208542b1222a2370
Masato Hatayama <masato.hatayama@gmail.com>
2020-07-16 17:24:07 +0900

    submit 2

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,19 +1,18 @@
-DICT_SCORES = {
+SCORES = {
     'AEIOULNRST': 1,
     'DG': 2,
     'BCMP': 3,
     'FHVWY': 4,
     'K': 5,
     'JX': 8,
-    'QZ': 10}
+    'QZ': 10
+}
+
+
+score_by_letters = {letter: score for (letter, score)
+                    in SCORES.items() for letter in letter}
 
 
 def score(word):
     chars = [x.upper() for x in word if x.isalpha()]
-    return sum(map(lambda x: _score_by_letter(x), chars))
-
-
-def _score_by_letter(letter):
-    for letters, score in DICT_SCORES.items():
-        if letter in letters:
-            return score
+    return sum(map(lambda x: score_by_letters[x], chars))
