d6b121415cd31d1887f64ee482b0009e14c9603a
Masato Hatayama <masato.hatayama@gmail.com>
2020-07-28 06:56:57 +0900

    submig

---
+++ grade-school/grade_school.py
@@ -0,0 +1,19 @@
+class School:
+    def __init__(self):
+        self.students_by_grade = {}
+
+    def add_student(self, name, grade):
+        if not grade in self.students_by_grade:
+            self.students_by_grade[grade] = [name]
+        else:
+            self.students_by_grade[grade].append(name)
+
+    def roster(self):
+        sorted_grades = sorted(self.students_by_grade.keys())
+        students_sorted_by_grade = [self.grade(grade) for grade in sorted_grades]
+        return [item for sublist in students_sorted_by_grade for item in sublist]
+
+    def grade(self, grade_number):
+        if not grade_number in self.students_by_grade:
+            return []
+        return sorted(self.students_by_grade[grade_number])
