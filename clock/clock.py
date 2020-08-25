3711eba11eab378cee66bda3e5b7e74310bce9bc
Masato Hatayama <masato.hatayama@gmail.com>
2020-08-26 08:22:07 +0900

    refactoring

--- clock/clock.py
+++ clock/clock.py
@@ -1,19 +1,31 @@
-from math import floor 
+from math import floor
+
 
 class Clock:
+    MINUTES_PER_HOUR = 60
+    HOURS_PER_DAY = 24
+    MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY
+
     def __init__(self, hour, minute):
-        self.minutes = hour * 60 + minute
+        self.minutes = (hour * self.MINUTES_PER_HOUR +
+                        minute) % self.MINUTES_PER_DAY
 
     def __repr__(self):
-        hour = floor(self.minutes / 60) % 24
-        minute = self.minutes % 60
-        return f'{hour:02}:{minute:02}'
+        return f'{self.hour:02}:{self.minute:02}'
 
     def __eq__(self, other):
-        return str(self) == str(other)
+        return self.minutes == other.minutes
 
     def __add__(self, minutes):
         return Clock(0, self.minutes + minutes)
 
     def __sub__(self, minutes):
         return Clock(0, self.minutes - minutes)
+
+    @property
+    def hour(self):
+        return floor(self.minutes / self.MINUTES_PER_HOUR) % self.HOURS_PER_DAY
+
+    @property
+    def minute(self):
+        return self.minutes % self.MINUTES_PER_HOUR
